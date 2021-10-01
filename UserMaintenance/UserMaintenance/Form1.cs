7c57915d93ce89972f953b870587740d83e1be40
Kíra Szabó <kira.szabo2@stud.uni-corvinus.hu>
2021-10-01 18:19:58 +0200

    3.commit

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,22 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList< User> users = new BindingList< User>();
         public Form1()
         {
             InitializeComponent();
             label2.Text = Resource.LastName; // label1
             label2.Text = Resource.FirstName; // label2
             button1.Text = Resource.Add; // button1
+
+            // ...
+
+            // listbox1
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
         }
+       
+
+      
+            
+      
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = LastName.Text,
+                FirstName = FirstName.Text
+            };
+            users.Add(u);
+
+
+
+
+
+        }
+        
+
+            
+        }
+           
         }
     }
 }
