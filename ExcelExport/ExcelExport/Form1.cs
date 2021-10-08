cb5f5aba7b66eaa7820cf01683d9fd5ea63b91bd
Kíra Szabó <kira.szabo2@stud.uni-corvinus.hu>
2021-10-08 17:56:12 +0200

    cell

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -55,15 +55,59 @@ namespace ExcelExport
         }
         private void CreateTable()
         {
+            string[] Headers = new string []
+            {
+                "Kód",
+     "Eladó",
+     "Oldal",
+     "Kerület",
+     "Lift",
+     "Szobák száma",
+     "Alapterület (m2)",
+     "Ár (mFt)",
+     "Négyzetméter ár (Ft/m2)"};
+            for (int i = 0; i < Headers.Length; i++)
+            
+                xlSheet.Cells[1, i+1] = Headers[i];
+
+            object[,] values = new object[lakasok.Count, Headers.Length];
+
+            int counter = 0;
+            foreach (var lakas in lakasok)
+            {
+                values[counter, 0] = lakas.Code;
+                // ...
+                values[counter, 8] = "";
+
+                counter++;
+            }
+            xlSheet.get_Range(
+             GetCell(2, 1),
+             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
 
 
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
 
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
 
 
 
 
 
-}
+    }
     
 }
