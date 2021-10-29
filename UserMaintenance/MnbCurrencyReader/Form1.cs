f56bcb87fca89c30b7e4b3078f588586aba4307a
Kíra Szabó <kira.szabo2@stud.uni-corvinus.hu>
2021-10-29 22:20:27 +0200

    5

--- UserMaintenance/MnbCurrencyReader/Form1.cs
+++ UserMaintenance/MnbCurrencyReader/Form1.cs
@@ -20,6 +20,7 @@ namespace MnbCurrencyReader
             InitializeComponent();
             Consume();
             dataGridView1.DataSource = Rates;
+            Diagram();
         }
 
         void Consume()
@@ -38,6 +39,24 @@ namespace MnbCurrencyReader
             var response = mnbService.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
+        }
+        void Diagram()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
