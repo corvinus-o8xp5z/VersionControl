b17641a44fb9965057456dc81707f008b4f82dfc
Kíra Szabó <kira.szabo2@stud.uni-corvinus.hu>
2021-10-29 22:05:35 +0200

    1&2

---
+++ UserMaintenance/MnbCurrencyReader/Form1.cs
@@ -0,0 +1,40 @@
+﻿using MnbCurrencyReader.MNBServiceReference;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MnbCurrencyReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Consume();
+        }
+
+        void Consume()
+        {
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+        }
+    }
+}
